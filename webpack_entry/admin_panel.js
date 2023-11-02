bb400d19e73b6b6c2e62d2d91c6ca45c94d05b0a
AlexanderKamynin <kamyninsasa@gmail.com>
2023-11-02 23:34:08 +0300

    initial commit x2

--- webpack_entry/admin_panel.js
+++
@@ -1,4 +0,0 @@
-require("../src/styles/admin_panel.less");
-require("../js_webpack/admin_panel.js");
-require("../js_webpack/request.js");
-require("../js_webpack/jquery.js");
--- webpack_entry/user.js
+++
@@ -1,4 +0,0 @@
-require("../src/styles/user.less");
-require("../js_webpack/jquery.js");
-require("../js_webpack/request.js");
-require("../js_webpack/user.js");
